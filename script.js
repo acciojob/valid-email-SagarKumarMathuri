e30a02040020ad6654fed8ec68b95b3d677d5a95
sagar kumar mathuri <sagarku2002@gmail.com>
2026-04-27 09:47:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// function validEmail(str) {
+//   //your JS code here.
+// }
+
+// // Do not change the code below.
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
 function validEmail(str) {
-  //your JS code here.
+  let regex = /^[\w.-]+@[\w-]+(\.[\w-]{2,3})+$/;
+  return regex.test(str);
 }
 
 // Do not change the code below.
